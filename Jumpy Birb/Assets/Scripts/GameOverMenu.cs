e260f26e18700d193bdbca7820c46e583878cbb7
HampusOls1203 <hammol1203@skola.goteborg.se>
2022-03-25 08:36:40 +0100

    GameoverMenu Buttons

---
+++ Jumpy Birb/Assets/Scripts/GameOverMenu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverMenu : MonoBehaviour
+{
+    public void MainMenuReturn()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
